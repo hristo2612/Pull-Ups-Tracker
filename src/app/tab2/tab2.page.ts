0eabccea0e78996c3abef881103b5d5603e6d7e2
hristo2612 <hristoapps@gmail.com>
2020-06-17 22:24:03 +0300

    Fix history today

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -19,9 +19,18 @@ export class Tab2Page {
         this.history = val;
         val.forEach((item, index) => {
           const timeAgo = new TimeAgo('en-US');
-          this.history[index] = { ...this.history[index], date: timeAgo.format(new Date(item.date)) }
+          this.history[index] = { ...this.history[index], date: this.getCurrentDay() === item.date ? 'Today' : timeAgo.format(new Date(item.date)) }
         });
       }
     });
+  }
+
+  getCurrentDay() {
+    let today: any = new Date();
+    let dd = String(today.getDate()).padStart(2, '0');
+    let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    let yyyy = today.getFullYear();
+
+    return today = mm + '/' + dd + '/' + yyyy;
   }
 }
